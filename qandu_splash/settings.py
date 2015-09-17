121159c76abf1f4bdb8e673dbfaf35bddf2042e8
AStengel54 <amstengel@quinnipiac.edu>
2015-09-17 13:43:04 +0000

    Setup Heroku

--- qandu_splash/settings.py
+++ qandu_splash/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+ON_HEROKU = os.getenv('ON_HEROKU', False)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MAIN_DIR= os.path.dirname(os.path.dirname(__file__))
@@ -24,7 +25,11 @@ MAIN_DIR= os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'o5ne=m4)**(2u$v=+ti%7+ukbigvrbdy1w0q-)=%zxbus!6(zz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ON_HEROKU == True: 
+  DEBUG = False
+else:
+  DEBUG = True
+ 
 
 ALLOWED_HOSTS = []
 
@@ -103,3 +108,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(MAIN_DIR, 'static'),)
+STATIC_ROOT = 'staticfiles'
